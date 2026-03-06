af6f3dcd225dfdf3ca7d0d8d734f0ab10693bd34
David Sarrat González <113605621+davidsarratgonzalez@users.noreply.github.com>
2026-03-06 06:02:57 +0100

    Add negotiation flow diagrams for HTTPS and plain HTTP

---
+++ assets/slidev/default-CfkMwsu5.js
@@ -0,0 +1,1 @@
+import{o,e as t,i as s}from"../modules/vue-C_D6JJH2.js";import{_ as r}from"../index-DmSFC5sh.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
